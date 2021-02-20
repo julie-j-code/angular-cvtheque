985dfaea4b28c596df2c69a3f8a4dae383c24918
Julie J <jeannet.julie@gmail.com>
2021-02-20 19:27:23 +0100

    création des services cv et embauche

---
+++ src/app/cvTech/cv.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Personne } from '../Model/Personne';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CvService {
+  private personnes: Personne[];
+
+  constructor() {
+    this.personnes = [
+      new Personne(1, "Adkins", "Julia", 38, "rotating_card_profile.png", 7777, "WebDesigner", " Phasellus at lorem a magna luctus volutpat. Morbi a dolor nunc. Ut non dictum turpis. Donec ac mollis ante, sit amet tempus arcu. Suspendisse eu pulvinar orci. "),
+      new Personne(2, "Delvot", "Christophe", 42, "rotating_card_profile2.png", 888, "Teacher", "Aliquam nec turpis vitae libero molestie fringilla. Etiam aliquet convallis elit, nec molestie tellus rhoncus nec. Nulla nisl risus, aliquet ut ante eu, pulvinar imperdiet diam."),
+      new Personne(3, "Dupeyret", "Franck", 33, "rotating_card_profile3.png", 7777, "Product Owner", " Phasellus at lorem a magna luctus volutpat. Morbi a dolor nunc. Ut non dictum turpis. Donec ac mollis ante, sit amet tempus arcu. Suspendisse eu pulvinar orci. ")
+    ];
+  }
+
+  getPersonnes() {
+    this.personnes;
+  }
+}
---
+++ src/app/cvTech/embauche.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { isJSDocThisTag } from 'tsd/libraries/typescript';
+import { Personne } from '../Model/Personne';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmbaucheService {
+  private personnes: Personne[]
+
+  constructor() {
+    this.personnes = [];
+  }
+
+  getEmbauches(){
+    return this.personnes;
+  }
+
+  embaucher(personne:Personne) : void{
+    const index = this.personnes.indexOf(personne);
+    if(index>=0){
+      this.personnes.push(personne)
+    }else{
+      alert(`${personne.name} est déjà sélectionné(e)`)
+    }
+  }
+
+  debaucher(personne:Personne) : void{
+    const index = this.personnes.indexOf(personne);
+    if(index>=0){
+      this.personnes.splice(index, 1);
+    }
+  }
+
+
+}
